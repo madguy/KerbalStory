1ee34bc09f564458b342f24019802368a6a74436
madguy <madguy@drumcan.in>
2015-10-08 09:13:21 +0900

    ダイアログを見やすいように変更

--- KerbalStory/StoryDialog.cs
+++ KerbalStory/StoryDialog.cs
@@ -4,37 +4,49 @@
 
 	internal class StoryDialog {
 		public static readonly String LOCK_ID = "9382013920";
+		private static readonly Int32 WINDOW_ID = 280391;
+
+		public String Title { get; set; }
 
 		public String Message { get; set; }
-		public Int32 Width { get; set; }
-		public Int32 Height { get; set; }
+
+		private KerbalInstructor instructor;
+
+		public KerbalInstructor Instructor {
+			get {
+				return this.instructor;
+			}
+			set {
+				this.instructor = value;
+				this.instructorTexture = new RenderTexture(this.instructorPortraitSize, this.instructorPortraitSize, 8);
+				this.instructor.instructorCamera.targetTexture = this.instructorTexture;
+			}
+		}
+
+		public String InstructorName { get; set; }
 
 		public event Action OnClick;
 
-		private KerbalInstructor instructor;
 		private Int32 instructorPortraitSize = 128;
 		private Int32 textureBorderRadius = 124;
 		private Rect instructorRect;
 		private RenderTexture instructorTexture;
 
-		private Rect windowPosition = new Rect(300, 60, 600, 400);
+		private Rect windowPosition = new Rect(300f, 150f, 600f, 500f);
 		private GUIStyle windowStyle;
 		private GUIStyle labelStyle = new GUIStyle(HighLogic.Skin.label);
-		private GUIStyle buttonStyle = new GUIStyle(HighLogic.Skin.button);
 		private GUIStyle scrollStyle = new GUIStyle(HighLogic.Skin.scrollView);
 		private Vector2 scrollPos = Vector2.zero;
 		private Boolean isVisible = false;
 
 		public StoryDialog() {
-			this.windowStyle = new GUIStyle(HighLogic.Skin.window);
-			this.windowStyle.fixedWidth = 600f;
-			this.windowStyle.fixedHeight = 400f;
-			this.Width = 580;
-			this.Height = 350;
+			var hoge = new GUILayoutOption[0];
+			this.windowStyle = new GUIStyle() {
+				fixedWidth = 400f,
+				fixedHeight = 600f,
+			};
 
-			this.instructor = Instructors.Wernher;
-			this.instructorTexture = new RenderTexture(this.instructorPortraitSize, this.instructorPortraitSize, 8);
-			this.instructor.instructorCamera.targetTexture = this.instructorTexture;
+			this.Instructor = Instructors.Wernher;
 		}
 
 		public void Show() {
@@ -58,7 +70,7 @@
 		}
 
 		private void OnDraw() {
-			this.windowPosition = GUILayout.Window(11, windowPosition, OnWindow, "Kerbal Story", windowStyle);
+			this.windowPosition = KSPUtil.ClampRectToScreen(GUILayout.Window(WINDOW_ID, this.windowPosition, this.OnWindow, this.Title));
 		}
 
 		private void OnWindow(Int32 windowId) {
@@ -71,10 +83,11 @@
 					if (Event.current.type == EventType.Repaint) {
 						var rect = GUILayoutUtility.GetLastRect();
 						this.instructorRect = new Rect(rect.x + 1f, rect.y + 1f, rect.width - 2f, rect.height - 2f);
-						Graphics.DrawTexture(this.instructorRect, this.instructorTexture, new Rect(0f, 0f, 1f, 1f), this.textureBorderRadius, this.textureBorderRadius, this.textureBorderRadius, this.textureBorderRadius, Color.white, this.instructor.PortraitRenderMaterial);
+						Graphics.DrawTexture(this.instructorRect, this.instructorTexture, new Rect(0f, 0f, 1f, 1f), this.textureBorderRadius, this.textureBorderRadius, this.textureBorderRadius, this.textureBorderRadius, Color.white, this.Instructor.PortraitRenderMaterial);
 					}
 
-					GUILayout.Label(this.instructor.CharacterName, this.labelStyle, GUILayout.Width(charaRectSize));
+					var instructorName = this.InstructorName ?? this.Instructor.CharacterName;
+					GUILayout.Label(instructorName, this.labelStyle, GUILayout.Width(charaRectSize));
 				}
 				GUILayout.EndVertical();
 
@@ -98,7 +111,7 @@
 			GUI.DragWindow();
 		}
 
-		private class Instructors {
+		public class Instructors {
 			public static KerbalInstructor Gene { get { return GetInstructor("Instructor_Gene"); } }
 			public static KerbalInstructor Wernher { get { return GetInstructor("Instructor_Wernher"); } }
 
